5fe905b4b073d01228da6707ebb46d4aff078212
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 00:43:59 -0400

    implemented the hiding and showing of the profile menu

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,12 +3,17 @@ import "./Profile.css";
 import defaultPicture from "./../../images/default.jpg";
 
 function Profile() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="Profile">
       <div className="Profile-image">
-        <img src={defaultPicture} alt="default profile" />
+        <img onClick={toggleMenu} src={defaultPicture} alt="default profile" />
       </div>
-      <div className="Profile-drop-down">
+      <div className={isMenuOpen ? "Profile-drop-down showMenu" : "Profile-drop-down hideMenu"}>
         <ul>
           <li>
             <span className="material-icons">person</span>Profile
