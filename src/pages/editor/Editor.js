d1f4114069fa4751801cac5328c59438d8b6ebca
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 11:08:38 -0400

    created Editor page Component

---
+++ src/pages/editor/Editor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Editor.css";
+import EditorNavigation from "./../../components/editor_navigation/EditorNavigation";
+
+function Editor() {
+  return (
+    <div className="Editor">
+      <EditorNavigation />
+    </div>
+  );
+}
+
+export default Editor;
