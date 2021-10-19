60c0e16e34454d486c3987516bfdeedde4ac75ff
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 10:52:59 -0400

    created SaveButton Component

---
+++ src/components/save_button/SaveButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SaveButton.css";
+
+function SaveButton() {
+  return (
+    <div className="SaveButton">
+      <button type="button" className="SaveButton-button">
+        <span class="material-icons">save</span>
+        <span className="SaveButton-text">Save</span>
+      </button>
+    </div>
+  );
+}
+
+export default SaveButton;
