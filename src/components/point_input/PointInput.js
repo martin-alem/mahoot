ba9c658388153d06f9767644f3a776ccbaa66d29
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 20:07:21 -0400

    created the PointInput component

---
+++ src/components/point_input/PointInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./PointInput.css";
+
+function PointInput() {
+  return (
+    <div className="PointInput">
+      <div className="PointInput-label">
+        <span className="material-icons">sports_score</span>
+        <span className="PointInput-text">Points</span>
+      </div>
+      <select name="point">
+        <option selected value="standard">
+          Standard
+        </option>
+        <option value="double">Double</option>
+        <option value="triple">Triple</option>
+      </select>
+    </div>
+  );
+}
+
+export default PointInput;
