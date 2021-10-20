72b2c96780fff6365824ab906ac9cd5cbf84cb61
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-20 15:46:42 -0400

    implemented a function to extract url params from url and also implemented an httpAgent to make calls to backend service

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,10 +3,18 @@ import "./Login.css";
 import GithubButton from "./../../components/github_button/GithubButton";
 import LinkedInButton from "./../../components/linkedin_button/LinkedInButton";
 import Logo from "../../components/logo/Logo";
+import { extractSearchParams, httpAgent } from "./../../utils/util";
 
-function Login(props)
-{
-  const params = props.match.params;
+function Login(props) {
+  const params = extractSearchParams(props.location.search);
+  const [state] = React.useState(params);
+  React.useEffect(() => {
+    if (Object.keys(params).length > 0 && params["code"]) {
+      console.log(state);
+    } else {
+      console.log("No data");
+    }
+  }, [state]);
   return (
     <div className="Login">
       <div className="Login-logo">
---
+++ src/utils/util.js
@@ -0,0 +1,34 @@
+export const extractSearchParams = (search) => {
+  const queryString = {};
+  if (search !== "") {
+    search = search.slice(1); // extract the ? character at the front of the string
+    const params = search.split("&");
+    params.forEach((param) => {
+      const [name, value] = param.split("=");
+      queryString[name] = value;
+    });
+  }
+  return queryString;
+};
+
+export const httpAgent = (url, method, data) => {
+  const init = {
+    method: method,
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    // mode: "same-origin",
+    // credentials: "same-origin",
+    // cache: "default",
+    // redirect: "follow",
+    // referrer: "smack",
+    // referrerPolicy: "same-origin",
+  };
+
+  if (method !== "GET" && method !== "HEAD") {
+    init["body"] = JSON.stringify(data);
+  }
+
+  return fetch(url, init);
+};
