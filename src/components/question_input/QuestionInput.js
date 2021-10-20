232d93e5dd95d24526876b909abd4acc739c2ab2
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 21:24:58 -0400

    styled the QuestionInput component

--- src/components/question_input/QuestionInput.js
+++ src/components/question_input/QuestionInput.js
@@ -4,7 +4,7 @@ import "./QuestionInput.css";
 function QuestionInput() {
   return (
     <div className="QuestionInput">
-      <textarea className="QuestionInput-question"></textarea>
+      <textarea className="QuestionInput-question" placeholder="Start typing your question"></textarea>
     </div>
   );
 }
--- src/pages/editor/Editor.js
+++ src/pages/editor/Editor.js
@@ -3,6 +3,7 @@ import "./Editor.css";
 import EditorNavigation from "./../../components/editor_navigation/EditorNavigation";
 import QuestionContainer from "./../../components/question_container/QuestionContainer";
 import QuestionConfig from "../../components/question_config/QuestionConfig";
+import QuestionInput from "./../../components/question_input/QuestionInput";
 
 function Editor() {
   return (
@@ -12,7 +13,9 @@ function Editor() {
         <div className="Editor-questions">
           <QuestionContainer />
         </div>
-        <div className="Editor-edit"></div>
+        <div className="Editor-edit">
+          <QuestionInput />
+        </div>
         <div className="Editor-question-params">
           <QuestionConfig />
         </div>
