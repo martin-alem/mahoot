fcb6a1e19fc57c9bebe0550e1ca01798dd568e2b
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-19 21:08:22 -0400

    created the QuestionInput component

---
+++ src/components/question_input/QuestionInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./QuestionInput.css";
+
+function QuestionInput() {
+  return (
+    <div className="QuestionInput">
+      <textarea className="QuestionInput-question"></textarea>
+    </div>
+  );
+}
+
+export default QuestionInput;
