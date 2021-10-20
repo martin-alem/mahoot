68f22aba41eb3df2f525eb87c3d55d626805c72f
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-20 14:50:50 -0400

    prepare the linkedIn button component to request linkedIn OAuth2.0 screen

--- src/components/linkedin_button/LinkedInButton.js
+++ src/components/linkedin_button/LinkedInButton.js
@@ -2,9 +2,19 @@ import React from "react";
 import "./LinkedInButton.css";
 
 function LinkedInButton() {
+  const responseType = process.env.REACT_APP_RESPONSE_TYPE;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const redirectURI = process.env.REACT_APP_REDIRECT_URI;
+  const state = process.env.REACT_APP_STATE;
+  const scope = process.env.REACT_APP_SCOPE;
+
+  const login = () => {
+    const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectURI}&state=${state}&scope=${scope}`;
+    window.location.assign(url);
+  };
   return (
     <div className="LinkedInButton">
-      <button className="LinkedInButton-button" type="button">
+      <button onClick={login} className="LinkedInButton-button" type="button">
         <i className="fab fa-linkedin-in LinkedInButton-icon"></i>
         <span className="LinkedInButton-text">Continue with linkedin</span>
       </button>
