7a9a331c49d17fdd51ca3c7cef67986b245c23b4
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-21 21:36:35 -0400

    create and exit button working

--- src/components/create_button/CreateButton.js
+++ src/components/create_button/CreateButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./CreateButton.css";
+import { Link } from "react-router-dom";
 
 function CreateButton() {
   return (
     <div className="CreateButton">
-      <button className="CreateButton-button" type="button">
-        <span className="material-icons">add</span>
-        <span className="CreateButton-text">Create</span>
-      </button>
+      <Link to="/editor">
+        <button className="CreateButton-button" type="button">
+          <span className="material-icons">add</span>
+          <span className="CreateButton-text">Create</span>
+        </button>
+      </Link>
     </div>
   );
 }
--- src/components/exit_button/ExitButton.js
+++ src/components/exit_button/ExitButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./ExitButton.css";
+import { Link } from "react-router-dom";
 
 function ExitButton() {
   return (
     <div className="ExitButton">
-      <button className="ExitButton-button" type="button">
-        <span className="material-icons">close</span>
-        <span className="ExitButton-text">Exit</span>
-      </button>
+      <Link to="/home">
+        <button className="ExitButton-button" type="button">
+          <span className="material-icons">close</span>
+          <span className="ExitButton-text">Exit</span>
+        </button>
+      </Link>
     </div>
   );
 }
