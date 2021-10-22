93cdced4670333af380fedbfb0194e8b3fd0b5c7
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-22 08:42:46 -0400

    added the ability to change time limit and points

--- src/components/point_input/PointInput.js
+++ src/components/point_input/PointInput.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./PointInput.css";
-import { QuizContext } from "./../../contexts/quizContext";
+import { QuizContext, QuizActionContext } from "./../../contexts/quizContext";
 
 function PointInput() {
   const quizContext = React.useContext(QuizContext);
+  const quizActionContext = React.useContext(QuizActionContext);
   const { edit } = quizContext.isEditable;
+  const [points, setPoints] = React.useState("standard");
+  const handleSelectChange = (e) => {
+    setPoints(e.target.value);
+    quizActionContext.performEdit({ type: "point", value: e.target.value });
+  };
   return (
     <div className="PointInput">
       <div className="PointInput-label">
         <span className="material-icons">sports_score</span>
         <span className="PointInput-text">Points</span>
       </div>
-      <select disabled={!edit} className="PointInput-input" name="point">
+      <select onChange={handleSelectChange} disabled={!edit} className="PointInput-input" name="point">
         <option defaultValue value="standard">
           Standard
         </option>
--- src/components/time_limit_input/TimeLimitInput.js
+++ src/components/time_limit_input/TimeLimitInput.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./TimeLimitInput.css";
-import { QuizContext } from "./../../contexts/quizContext";
+import { QuizContext, QuizActionContext } from "./../../contexts/quizContext";
 
 function TimeLimitInput() {
   const quizContext = React.useContext(QuizContext);
+  const quizActionContext = React.useContext(QuizActionContext);
   const { edit } = quizContext.isEditable;
+  const [timeLimit, setTimeLimit] = React.useState(5);
+  const handleSelectChange = (e) => {
+    setTimeLimit(e.target.value);
+    quizActionContext.performEdit({ type: "duration", value: e.target.value });
+  };
   return (
     <div className="TimeLimitInput">
       <div className="TimeLimitInput-label">
         <span className="material-icons">timer</span>
         <span className="TimeLimitInput-text">Time Limit</span>
       </div>
-      <select disabled={!edit} className="TimeLimitInput-input" name="timeLimit">
+      <select onChange={handleSelectChange} disabled={!edit} className="TimeLimitInput-input" name="timeLimit">
         <option defaultValue value="5">
           5 Seconds
         </option>
