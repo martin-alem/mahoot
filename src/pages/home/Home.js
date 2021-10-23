d6affaa4a54e86656fff3278787d4176d4ad2821
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-23 13:39:05 -0400

    set up home component to pull quizes from server

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import HomeNavigation from "./../../components/home_navigation/HomeNavigation";
+import DisplayMessage from "./../../components/display_message/DisplayMessage";
 import Quiz from "./../../components/quiz/Quiz";
 import { UserContext, UserActionContext } from "../../contexts/userContext";
 import { httpAgent } from "./../../utils/util";
@@ -8,29 +9,34 @@ import { httpAgent } from "./../../utils/util";
 function Home() {
   const userContext = React.useContext(UserContext);
   const userActionContext = React.useContext(UserActionContext);
-  // React.useEffect(() => {
-  //   const url = "http://localhost:4000";
-  //   const method = "GET";
-  //   const data = {};
-  //   httpAgent(url, method, data)
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         response
-  //           .json()
-  //           .then((data) => {
-  //             // userActionContext.setLoggedIn(true);
-  //           })
-  //           .catch((error) => console.error(error));
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, []);
+  const [isFetching, setIsFetching] = React.useState(true);
+  const [quizzes, setQuizzes] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchQuiz = async () => {
+      try {
+        const url = "http://localhost:4000/api/v1/quiz";
+        const method = "GET";
+        const response = await httpAgent(url, method, {});
+        if (response.ok) {
+          const results = await response.json();
+          setIsFetching(false);
+          console.log(results);
+        }
+      } catch (error) {
+         setIsFetching(false);
+        userActionContext.setMessage({ visible: true, type: "error", message: "Unable to fetch quizzes." });
+        console.log(error);
+      }
+    };
+
+    fetchQuiz();
+  }, []);
   return (
     <div className="Home">
+      <DisplayMessage />
       <HomeNavigation />
-      <div className="Home-mahoot"></div>
+      <div className="Home-mahoot">{isFetching ? <p>Fetching quizzes...</p> : ""}</div>
     </div>
   );
 }
