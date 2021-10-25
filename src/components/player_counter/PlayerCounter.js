cd60cc09d927a0646f309d0cc73cfe5996230dc9
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-24 22:10:07 -0400

    created and implemented the player counter component and functionality

--- src/components/player_counter/PlayerCounter.js
+++ src/components/player_counter/PlayerCounter.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./PlayerCounter.css";
 
-function PlayerCounter()
-{
-    return (
-      <div className="PlayerCounter">
-            <span className="material-icons PlayerCounter-icon">person</span>
-            <span className="PlayerCounter-count">0</span>
-      </div>
-    );
+function PlayerCounter(props) {
+  return (
+    <div className="PlayerCounter">
+      <span className="material-icons PlayerCounter-icon">person</span>
+      <span className="PlayerCounter-count">{props.playerCount}</span>
+    </div>
+  );
 }
 
 export default PlayerCounter;
