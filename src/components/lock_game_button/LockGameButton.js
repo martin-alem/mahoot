6870ae2367b18dd74ddc8b32fae161d0c670815a
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-24 22:09:29 -0400

    created and implemented the lock game button and functionality

--- src/components/lock_game_button/LockGameButton.js
+++ src/components/lock_game_button/LockGameButton.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./LockGameButton.css";
+import { GameContext } from "./../../contexts/gameContext";
+import { getFromLocalStorage, setToLocalStorage } from "./../../utils/util";
 
 function LockGameButton() {
+  const gameContext = React.useContext(GameContext);
+  const gameSocket = gameContext.socket;
+  const quizId = getFromLocalStorage("quizId");
+  const [gameLock, setGameLock] = React.useState(false);
+  const lockGame = () => {
+    gameSocket.send(JSON.stringify({ type: "room_state", quizId: quizId, state: gameLock }));
+  };
+
+  if (gameSocket) {
+    gameSocket.addEventListener("message", (event) => {
+      console.log(event.data);
+      const msg = JSON.parse(event.data);
+      if (msg.type === "room_state") {
+        setGameLock(!msg.state);
+      }
+    });
+  }
+
   return (
     <div className="LockGameButton">
-      <button type="button" className="LockGameButton-button">
-        <span className="material-icons LockGameButton-text">lock_open</span>
+      <button onClick={lockGame} type="button" className="LockGameButton-button">
+        <span className="material-icons LockGameButton-text">{gameLock ? "lock" : "lock_open"}</span>
       </button>
     </div>
   );
