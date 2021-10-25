710bfff03d998212ae786de3626dafd53952e064
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-24 22:11:31 -0400

    connected the lobby to the socket server on render

--- src/pages/lobby/Lobby.js
+++ src/pages/lobby/Lobby.js
@@ -4,21 +4,52 @@ import PlayerCounter from "./../../components/player_counter/PlayerCounter";
 import StartGameButton from "./../../components/start_game_button/StartGameButton";
 import EndGameButton from "./../../components/end_game_button/EndGameButton";
 import LockGameButton from "./../../components/lock_game_button/LockGameButton";
+import { getFromLocalStorage, setToLocalStorage } from "./../../utils/util";
+import {GameContext} from "./../../contexts/gameContext"
 
-function Lobby(props)
+function Lobby()
 {
+  const gameContext = React.useContext(GameContext);
+  const [players, setPlayers] = React.useState([]);
+  React.useEffect(() => {
+    const quizId = getFromLocalStorage("quizId");
+    const socket = new WebSocket(`ws://localhost:8080/api/admin`);
+    gameContext.setSocket(socket);
+    socket.addEventListener("open", (event) =>
+    {
+      
+      const connectMessage = { type: "create", quizId: quizId };
+      socket.send(JSON.stringify(connectMessage));
+    });
+
+    socket.addEventListener("message", (event) => {
+      const msg = JSON.parse(event.data);
+      if (msg.type === "code") {
+        setToLocalStorage({ key: "roomId", value: msg.gameCode });
+        document.querySelector(".Game-pin").textContent = msg.gameCode;
+      } else if (msg.type === "join") {
+        setPlayers((prevState) => {
+          return [...prevState, { nickname: msg.nickname }];
+        });
+      }
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("Connection closed");
+    });
+  }, []);
   return (
     <div className="Lobby">
       <div className="Lobby-header">
         <p>
           Join at <span className="Game-url">mahoot.io/play</span> with Game PIN:
         </p>
-        <p className="Game-pin">658 653 458</p>
+        <p className="Game-pin">{getFromLocalStorage("roomId")}</p>
       </div>
       <div className="Lobby-main">
         <div className="Lobby-nav">
           <div className="Lobby-player-counter">
-            <PlayerCounter />
+            <PlayerCounter playerCount={players.length} />
           </div>
           <div className="Lobby-action">
             <LockGameButton />
@@ -27,7 +58,15 @@ function Lobby(props)
           </div>
         </div>
         <div className="Lobby-players">
-          <p className="Lobby-waiting">Waiting for players...</p>
+          <p className="Lobby-waiting">
+            {players.length === 0
+              ? "Waiting for players..."
+              : players.map((player, index) => (
+                  <span className="Lobby-player" key={index}>
+                    {player.nickname}
+                  </span>
+                ))}
+          </p>
         </div>
       </div>
     </div>
